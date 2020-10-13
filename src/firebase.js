3b59adb57e8a05f34267283ad1dfe6efaec541dc
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-13 09:47:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFPVfd9pDrNkkX2uIXAbXK3Rcmh9EXFNM",
-    authDomain: "facebook-clone-ece17.firebaseapp.com",
-    databaseURL: "https://facebook-clone-ece17.firebaseio.com",
-    projectId: "facebook-clone-ece17",
-    storageBucket: "facebook-clone-ece17.appspot.com",
-    messagingSenderId: "92698122307",
-    appId: "1:92698122307:web:c98841b2801e326cfc9cf6",
-    measurementId: "G-T27W50XQ4Q",
+    
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
